52dc2e9c49cf696197e7cd71fcff46c5a0c78734
Meg Sandman <margaret.sandman@gmail.com>
2015-07-03 10:35:44 -0700

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
 
+
+  resources :cities, :only => [:index] do
+    resources :neighborhoods, :only => [:index]
+  end
+
+  resources :activities, :only => [:index] do
+    resources :genres, :only => [:index]
+  end
+
+  resources :matches, :only => [:index, :show, :update, :new, :create]
+  # resources :venues, :only => []
+
+  # resources :websites, :only => [:index]
+  # resources :imports, :only => [:index]
+  # root to: 'websites#index'
+
 end
