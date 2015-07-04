6edab3147068788c8b0bcf189756b7f9fa7159c1
Meg Sandman <margaret.sandman@gmail.com>
2015-07-03 17:31:55 -0700

    neighborhoods#index and genres#index routes

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,14 @@
 class GenresController < ApplicationController
+
+  def index
+    if params[:activity_id]
+      @genres = Genre.where(activity_id: params[:activity_id]).order(:name)
+    else
+      @genres = Genre.all.order(:name)
+    end
+
+    render :json => @genres, status: 200
+  end
+
 end
+
--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -1,2 +1,13 @@
 class NeighborhoodsController < ApplicationController
+
+  def index
+    if params[:city_id]
+      @neighborhoods = Neighborhood.where(city_id: params[:city_id]).order(:name)
+    else
+      @neighborhoods = Neighborhood.all.order(:name)
+    end
+
+    render :json => @neighborhoods, status: 200
+  end
+
 end
